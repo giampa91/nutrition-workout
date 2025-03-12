b4cea37240f5e3aba13fc850fe416c6d5ae9a915
giampaolo <giampaolo.orru@paranor.ch>
2025-03-12 23:15:46 +0100

    create execution service

---
+++ workout-execution-service/src/test/java/com/nutrition/workout/workoutexecutionservice/WorkoutExecutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrition.workout.workoutexecutionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutExecutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workout-execution-service/workout-execution-service/src/main/java/com/nutrition/workout/workoutexecutionservice/WorkoutExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition.workout.workoutexecutionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutExecutionServiceApplication.class, args);
+	}
+
+}
