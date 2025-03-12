4268d706a9ee1e99ee4dc5cd2996673d4e02709a
giampaolo <giampaolo.orru@paranor.ch>
2025-03-12 18:48:57 +0100

    insert first microservices

---
+++ meal-tracking-service/src/main/java/com/nutrition/workout/mealtrackingservice/MealTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition.workout.mealtrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealTrackingServiceApplication.class, args);
+	}
+
+}
