351d15c29e8b718c29e99f60f50948cce95af5f3
giampaolo <giampaolo.orru@paranor.ch>
2025-03-13 16:05:50 +0100

    update eureka server in order to work with all services

--- workout-execution-service/src/main/java/com/nutrition/workout/workoutexecutionservice/kafka/WorkoutConsumerService.java
+++ workout-execution-service/src/main/java/com/nutrition/workout/workoutexecutionservice/kafka/WorkoutConsumerService.java
@@ -1,4 +1,13 @@
 package com.nutrition.workout.workoutexecutionservice.kafka;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WorkoutConsumerService {
+
+    @KafkaListener(topics = "workout-topic", groupId = "workout-consumer-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
 }
